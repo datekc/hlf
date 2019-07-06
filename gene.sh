71502839edc312669be07a905fdb8e06ad172790
datekc <51317660+datekc@users.noreply.github.com>
2019-07-06 16:05:47 +0530

    Add files via upload

---
+++ gene.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export FABRIC_CFG_PATH=${PWD}
+export PATH=${PWD}:$PATH
+echo $FABRIC_CFG_PATH
+configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
