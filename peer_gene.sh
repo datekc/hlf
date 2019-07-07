f2325a5efc0c2c69dd184c32a3417720082830d3
datekc <51317660+datekc@users.noreply.github.com>
2019-07-07 15:01:06 +0530

    added peer_gene.sh

---
+++ peer_gene.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export FABRIC_CFG_PATH=${PWD}
+export PATH=${PWD}:$PATH
+echo $FABRIC_CFG_PATH
+# generate channel configuration transaction
+configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
